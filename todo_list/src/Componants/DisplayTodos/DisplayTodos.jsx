2efa1392a5a87c0eb608f0d86b01fce0c052b048
Kishore4f2 <satyakishore273@gmail.com>
2026-03-16 19:06:02 +0530

    Delete and Mark as Completed

--- todo_list/src/Componants/DisplayTodos/DisplayTodos.jsx
+++ todo_list/src/Componants/DisplayTodos/DisplayTodos.jsx
@@ -5,32 +5,61 @@ function DisplayTodos() {
 
   const [tasks, setTasks] = useState([]);
 
+  const OnMarkedBtn = async(id)=>{
+    try {
+        let res2 = await fetch(`http://localhost:3001/todos/${id}`);
+        let todo = await res2.json();
+
+        let res3 = await fetch(`http://localhost:3001/todos/${id}`,{
+          method : "PATCH",
+          headers : {
+            "Content-Type" : "application/json" ,
+          },
+          body : JSON.stringify({
+            isCompleted: !todo.isCompleted
+          })
+        });
+
+        let updatedTodo = await res3.json();
+
+        setTasks(tasks.map(task=>task.id === id?updatedTodo : task));
+        
+            console.log("Todo Updated Succesfully")
+          
+      } catch (error) {
+        console.log(error.message);
+      }
+  }
+
+  const OnDeleteBtn = async(id)=>{
+    try {
+        let res1 = await fetch(`http://localhost:3001/todos/${id}`, {
+          method : "DELETE"
+          });
+        let jsonRes1 = await res1.json();
+          if(jsonRes1.ok) {
+            console.log("Todo Deleted Succesfully")
+          }
+      } catch (error) {
+        console.log(error.message);
+      }
+  }
+
   useEffect(() => {
 
     async function getAllTodos() {
 
       try {
-
         let user_id = localStorage.getItem("login_user");
-
-        let res = await fetch(
-          `http://localhost:3001/todos?user_id=${user_id}`
-        );
-
+        let res = await fetch(`http://localhost:3001/todos?user_id=${user_id}`);
         let jsonRes = await res.json();
-
         setTasks(jsonRes);
-
       } catch (error) {
-
         console.log(error.message);
-
       }
     }
-
     getAllTodos();
-
-  }, []);
+  }, [tasks]);
 
   return (
 
@@ -46,9 +75,9 @@ function DisplayTodos() {
 
           <div className="todo-buttons">
 
-            <button>Delete</button>
+            <button onClick={()=>OnDeleteBtn(item.id)}>Delete</button>
 
-            <button>
+            <button onClick={()=>OnMarkedBtn(item.id)}>
               {item.isCompleted
                 ? "Completed"
                 : "Mark as Completed"}
